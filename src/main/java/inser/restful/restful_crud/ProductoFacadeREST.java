b0475747b8f088c03f9f835a653847156a6334d0
informatica <informatica@informatica-VirtualBox>
2021-12-24 12:21:57 +0000

    Version 1.0 Jakarta

---
+++ src/main/java/inser/restful/restful_crud/ProductoFacadeREST.java
@@ -0,0 +1,151 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inser.restful.restful_crud;
+
+import inser.persistence.restful_crud.LinkedList_envuelta;
+import static inser.persistence.restful_crud.PersistenceConfig.jdbc_contraseña;
+import static inser.persistence.restful_crud.PersistenceConfig.jdbc_usuario;
+import inser.persistence.restful_crud.Producto;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import jakarta.persistence.EntityManager;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.MediaType;
+import java.util.LinkedList;
+
+/**
+ *
+ * @author informatica
+ */
+@jakarta.ejb.Stateless
+@Path("inser.restful.restful_crud.producto")
+public class ProductoFacadeREST extends AbstractFacade<Producto> {
+    @Context
+    // opción 1: private UriInfo context;
+    // opción 2: 
+    private HttpServletRequest context;
+    
+//    @PersistenceContext(unitName = "restful_crudPU")
+    public EntityManager entityManager;
+    public Map<String, String> propiedades_mapa = new HashMap();
+
+    public ProductoFacadeREST() {
+        super(Producto.class);
+    }
+
+    @POST
+    @Override
+    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public void create(Producto entity) {
+        String texto;     
+        texto = context.getHeader(jdbc_usuario);
+        propiedades_mapa.put(jdbc_usuario, texto);
+        texto = context.getHeader(jdbc_contraseña);        
+        propiedades_mapa.put(jdbc_contraseña, texto);
+        super.create(entity);
+    }
+
+    @PUT
+    @Path("{id}")
+    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public void edit(@PathParam("id") String id, Producto entity) {
+        String texto;     
+        texto = context.getHeader(jdbc_usuario);
+        propiedades_mapa.put(jdbc_usuario, texto);
+        texto = context.getHeader(jdbc_contraseña);        
+        propiedades_mapa.put(jdbc_contraseña, texto);
+        super.edit(entity);
+    }
+
+    @DELETE
+    @Path("{id}")
+    public void remove(@PathParam("id") String id) {
+        String texto;     
+        texto = context.getHeader(jdbc_usuario);
+        propiedades_mapa.put(jdbc_usuario, texto);
+        texto = context.getHeader(jdbc_contraseña);        
+        propiedades_mapa.put(jdbc_contraseña, texto);
+        Producto producto = super.find(id);
+        super.remove(producto);
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public Producto find(@PathParam("id") String id) {
+        String texto;     
+        texto = context.getHeader(jdbc_usuario);
+        propiedades_mapa.put(jdbc_usuario, texto);
+        texto = context.getHeader(jdbc_contraseña);        
+        propiedades_mapa.put(jdbc_contraseña, texto);
+        Producto producto = super.find(id);
+        return producto;
+    }
+
+    @GET
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public LinkedList_envuelta<Producto> findAll_envuelta() {
+        LinkedList_envuelta<Producto> linkedList_envuelta = new LinkedList_envuelta();
+        String texto;     
+        texto = context.getHeader(jdbc_usuario);
+        propiedades_mapa.put(jdbc_usuario, texto);
+        texto = context.getHeader(jdbc_contraseña);        
+        propiedades_mapa.put(jdbc_contraseña, texto);
+        List<Producto> productos_lista = new LinkedList<Producto>(super.findAll());
+        linkedList_envuelta.lista = new LinkedList(productos_lista);
+        return linkedList_envuelta;
+    }
+
+    @GET
+    @Path("{from}/{to}")
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public LinkedList_envuelta<Producto> findRange(@PathParam("from") Integer from, @PathParam("to") Integer to) {
+        LinkedList_envuelta<Producto> linkedList_envuelta = new LinkedList_envuelta();
+        String texto;     
+        texto = context.getHeader(jdbc_usuario);
+        propiedades_mapa.put(jdbc_usuario, texto);
+        texto = context.getHeader(jdbc_contraseña);        
+        propiedades_mapa.put(jdbc_contraseña, texto);
+        List<Producto> productos_lista = super.findRange(new int[]{from, to});
+        linkedList_envuelta.lista = new LinkedList(productos_lista);
+        return linkedList_envuelta;
+    }
+
+    @GET
+    @Path("count")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String countREST() {
+        String texto;     
+        texto = context.getHeader(jdbc_usuario);
+        propiedades_mapa.put(jdbc_usuario, texto);
+        texto = context.getHeader(jdbc_contraseña);        
+        propiedades_mapa.put(jdbc_contraseña, texto);
+        return String.valueOf(super.count());
+    }
+
+    @Override
+    protected EntityManager getEntityManager() {
+        String usuario;
+        String contraseña;
+        String [] error = { "" };
+        usuario = propiedades_mapa.get(jdbc_usuario);
+        contraseña = propiedades_mapa.get(jdbc_contraseña);
+        entityManager = ApplicationConfig.getEntityManager("restful_crudPU"
+                , usuario, contraseña, propiedades_mapa, error);
+        return entityManager;
+    }
+    
+}
