62f89990af3af41720978bdb60770a2a9d0166a3
informatica <informatica@informatica-VirtualBox>
2022-01-03 18:54:54 +0000

    v0.0

--- src/main/java/inser/restful/restful_crud/ProductoFacadeREST.java
+++ src/main/java/inser/restful/restful_crud/ProductoFacadeREST.java
@@ -9,10 +9,13 @@ import inser.persistence.restful_crud.LinkedList_envuelta;
 import static inser.persistence.restful_crud.PersistenceConfig.jdbc_contraseña;
 import static inser.persistence.restful_crud.PersistenceConfig.jdbc_usuario;
 import inser.persistence.restful_crud.Producto;
+import static inser.persistence.restful_crud.Producto.SELECT_findAll_order;
+import static inser.persistence.restful_crud.Producto.SELECT_findByCodigoProducto_order;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.DELETE;
@@ -146,6 +149,82 @@ public class ProductoFacadeREST extends AbstractFacade<Producto> {
         entityManager = ApplicationConfig.getEntityManager("restful_crudPU"
                 , usuario, contraseña, propiedades_mapa, error);
         return entityManager;
+    }
+
+    @GET
+    @Path("{from}/{to}/{descripcion}")
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public LinkedList_envuelta<Producto> findLike_descripcion(@PathParam("from") Integer from, @PathParam("to") Integer to, @PathParam("descripcion") String descripcion) {
+        LinkedList_envuelta<Producto> linkedList_envuelta = new LinkedList_envuelta();
+        List<Producto> productos_lista;
+        String texto;     
+        texto = context.getHeader(jdbc_usuario);
+        propiedades_mapa.put(jdbc_usuario, texto);
+        texto = context.getHeader(jdbc_contraseña);        
+        propiedades_mapa.put(jdbc_contraseña, texto);
+        TypedQuery<Producto> typedQuery = getEntityManager().createNamedQuery("Producto.findByCodigoProducto", Producto.class);
+        typedQuery = typedQuery.setParameter("descripcion", descripcion);
+        typedQuery = typedQuery.setMaxResults(to - from + 1);
+        typedQuery = typedQuery.setFirstResult(from);
+        productos_lista = typedQuery.getResultList();
+        linkedList_envuelta.lista = new LinkedList(productos_lista);
+        return linkedList_envuelta;
+    }
+    
+    @GET
+    @Path("{from}/{to}/{campo_ordenacion}/{asc}")
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public LinkedList_envuelta<Producto> find_orden(@PathParam("from") Integer from
+            , @PathParam("to") Integer to
+            , @PathParam("campo_ordenacion") String campo_ordenacion
+            , @PathParam("asc") String asc) {
+        List<Producto> productos_lista;
+        LinkedList_envuelta<Producto> linkedList_envuelta = new LinkedList_envuelta();
+        String texto;     
+        texto = context.getHeader(jdbc_usuario);
+        propiedades_mapa.put(jdbc_usuario, texto);
+        texto = context.getHeader(jdbc_contraseña);        
+        propiedades_mapa.put(jdbc_contraseña, texto);
+        String consulta = SELECT_findAll_order
+                + campo_ordenacion;
+        if (asc.toLowerCase().equals("desc")) {
+            consulta = consulta + " DESC";
+        }
+        TypedQuery<Producto> typedQuery = getEntityManager().createQuery(consulta, Producto.class);
+        typedQuery = typedQuery.setMaxResults(to - from + 1);
+        typedQuery = typedQuery.setFirstResult(from);
+        productos_lista = typedQuery.getResultList();
+        linkedList_envuelta.lista = new LinkedList(productos_lista);
+        return linkedList_envuelta;
+    }
+
+    @GET
+    @Path("{from}/{to}/{descripcion}/{campo_ordenacion}/{asc}")
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public LinkedList_envuelta<Producto> findLike_descripcion_orden(@PathParam("from") Integer from
+            , @PathParam("to") Integer to
+            , @PathParam("descripcion") String descripcion
+            , @PathParam("campo_ordenacion") String campo_ordenacion
+            , @PathParam("asc") String asc) {
+        LinkedList_envuelta<Producto> linkedList_envuelta = new LinkedList_envuelta();
+        List<Producto> productos_lista;
+        String texto;     
+        texto = context.getHeader(jdbc_usuario);
+        propiedades_mapa.put(jdbc_usuario, texto);
+        texto = context.getHeader(jdbc_contraseña);        
+        propiedades_mapa.put(jdbc_contraseña, texto);
+        String consulta = SELECT_findByCodigoProducto_order
+                + campo_ordenacion;
+        if (asc.toLowerCase().equals("desc")) {
+            consulta = consulta + " DESC";
+        }
+        TypedQuery<Producto> typedQuery = getEntityManager().createQuery(consulta, Producto.class);
+        typedQuery = typedQuery.setParameter("descripcion", descripcion);
+        typedQuery = typedQuery.setMaxResults(to - from + 1);
+        typedQuery = typedQuery.setFirstResult(from);
+        productos_lista = typedQuery.getResultList();
+        linkedList_envuelta.lista = new LinkedList(productos_lista);
+        return linkedList_envuelta;
     }
     
 }
